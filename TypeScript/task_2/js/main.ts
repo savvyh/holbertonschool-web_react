bee704176494d13d7e5428259292876c82ef5086
savvyh <7277@holbertonstudents.com>
2025-09-26 12:11:30 +0000

    feat: add 2 function to define if employee is director and one to execute work as director or teacher

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -40,3 +40,14 @@ function createEmployee(salary: number | string): Director | Teacher {
     }
     return new Director();
 }
+
+function isDirector(employee: Director | Teacher): employee is Director {
+    return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (employee instanceof Director) {
+        return employee.workDirectorTasks();
+    }
+    return employee.workTeacherTasks();
+}
