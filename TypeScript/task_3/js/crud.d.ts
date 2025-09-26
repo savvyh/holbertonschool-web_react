5eb0b852139e02dd0cf587de56fbac788a39cfe1
savvyh <7277@holbertonstudents.com>
2025-09-26 12:38:58 +0000

    feat: add CRUD function and build interface

---
+++ TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,6 @@
+import { RowID } from "./interface";
+import { RowElement } from "./interface";
+
+export function insertRow(row: RowElement): RowID;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId: RowID, row: RowElement): RowID;
