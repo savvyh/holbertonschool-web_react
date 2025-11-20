cf8306690bd4177d6e28728eb9d69cb2ffa704eb
savvyh <7277@holbertonstudents.com>
2025-11-20 20:58:47 +0000

    feat: initialize React project with Vite, ESLint configuration, and essential files for dashboard module

---
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import logo from './assets/holberton-logo.jpg'
+import { getCurrentYear, getFooterCopy } from './utils'
+import Notifications from './Notifications'
+
+function App() {
+  return (
+    <>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+      <div className="App-header">
+        <img src={logo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getCurrentYear()} - {getFooterCopy(false)}</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,11 @@
+export function getCurrentYear() {
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return 'Holberton School';
+  }
+  return 'Holberton School main dashboard';
+}
+
