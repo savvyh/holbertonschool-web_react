c5b4678c3465cc6b77c24dbaefa0d5aa83dfc014
savvyh <7277@holbertonstudents.com>
2025-11-21 08:35:36 +0000

    feat: copy task 2 to create task 3

---
+++ react_intro/task_3/dashboard/src/Notifications.jsx
@@ -0,0 +1,38 @@
+import './Notifications.css';
+import closeIcon from './assets/close-button.png';
+import { getLatestNotification } from './utils';
+
+function Notifications() {
+  const handleClick = () => {
+    console.log('Close button has been clicked');
+  };
+
+  return (
+    <div className="notification-items">
+      <button
+        type="button"
+        aria-label="Close"
+        onClick={handleClick}
+        style={{
+          position: 'absolute',
+          top: '10px',
+          right: '10px',
+          background: 'transparent',
+          border: 'none',
+          cursor: 'pointer',
+        }}
+      >
+        <img src={closeIcon} alt="close" />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;
+
