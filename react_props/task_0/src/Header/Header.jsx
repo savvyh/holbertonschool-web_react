b9247cfa8e7802ce8e394139569bcd47413427eb
savvyh <7277@holbertonstudents.com>
2025-11-21 22:57:38 +0000

    feat: create component header, footer and login

---
+++ react_props/task_0/src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css';
+import logo from '../assets/holberton-logo.jpg';
+
+function Header() {
+  return (
+    <div className="App-header">
+      <img src={logo} alt="holberton logo" />
+      <h1>School dashboard</h1>
+    </div>
+  );
+}
+
+export default Header;
