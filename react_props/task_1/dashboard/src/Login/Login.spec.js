24d8e27b0b57f140ea92813429b136b645366882
savvyh <7277@holbertonstudents.com>
2025-11-24 09:49:12 +0000

    feat: add login test

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -7,4 +7,26 @@ describe('Login Component', () => {
     const loginText = screen.getByText(/login to access the full dashboard/i);
     expect(loginText).toBeInTheDocument();
   });
+
+  test('includes 2 labels, 2 inputs and a button', () => {
+    const { container } = render(<Login />);
+    const labels = container.querySelectorAll('label');
+    expect(labels.length).toBe(2);
+    const inputs = container.querySelectorAll('input');
+    expect(inputs.length).toBe(2);
+    const button = container.querySelector('button');
+    expect(button).toBeInTheDocument();
+  });
+
+  test('inputs get focus when clicked', () => {
+    render(<Login />);
+    const emailInput = screen.getByLabelText(/email:/i);
+    const passwordInput = screen.getByLabelText(/password:/i);
+
+    emailInput.focus();
+    expect(emailInput).toHaveFocus();
+
+    passwordInput.focus();
+    expect(passwordInput).toHaveFocus();
+  });
 });
