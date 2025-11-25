f608957579d869f0e7c524dd72bcb224d7159e67
savvyh <7277@holbertonstudents.com>
2025-11-25 10:16:38 +0000

    feat: init task 3

---
+++ react_props/task_3/dashboard/src/Notifications/NotificationItem.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+
+function NotificationItem({ type = 'default', html = null, value = '' }) {
+  const styles = {
+    color: type === 'urgent' ? 'red' : 'blue'
+  };
+
+  if (html) {
+    return (
+      <li
+        data-notification-type={type}
+        style={styles}
+        dangerouslySetInnerHTML={html}
+      />
+    );
+  }
+
+  return (
+    <li
+      data-notification-type={type}
+      style={styles}
+    >
+      {value}
+    </li>
+  );
+}
+
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string
+  }),
+  value: PropTypes.string
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  html: null,
+  value: ''
+};
+
+export default NotificationItem;
