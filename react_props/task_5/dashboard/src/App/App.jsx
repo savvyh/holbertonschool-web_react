7e9d9bd75107892de4afb3e5887864b474f13be8
savvyh <7277@holbertonstudents.com>
2025-11-25 13:17:39 +0000

    feat: init task 5

---
+++ react_props/task_5/dashboard/src/App/App.jsx
@@ -0,0 +1,73 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import PropTypes from 'prop-types';
+
+function App({ isLoggedIn = false }) {
+  const notificationsList = [
+    {
+      id: 1,
+      type: 'urgent',
+      value: 'Notification available now'
+    },
+    {
+      id: 2,
+      type: 'urgent',
+      value: 'Notification available now'
+    },
+    {
+      id: 3,
+      type: 'urgent',
+      html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' }
+    }
+  ];
+
+  const coursesList = [
+    { 
+      id: 1,
+      name: 'ES6',
+      credit: 60
+    },
+    { 
+      id: 2,
+      name: 'Webpack',
+      credit: 20
+    },
+    { 
+      id: 3,
+      name: 'React',
+      credit: 40
+    }
+  ];
+
+  return (
+    <Fragment>
+      <div className="root-notifications">
+        <Notifications notifications={notificationsList} />
+      </div>
+      <Header />
+      <div className="App-body">
+        {isLoggedIn ? (
+          <CourseList courses={coursesList} />
+        ) : (
+          <Login />
+        )}
+      </div>
+      <Footer />
+    </Fragment>
+  );
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool
+};
+
+App.defaultProps = {
+  isLoggedIn: false
+};
+
+export default App;
