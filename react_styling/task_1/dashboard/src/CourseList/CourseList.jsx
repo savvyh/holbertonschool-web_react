0b3d0b801ee177b35cb31f609671495d9e2bad9c
savvyh <7277@holbertonstudents.com>
2025-11-26 14:29:08 +0000

    refactor: remove css file

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import CourseListRow from './CourseListRow';
-import './CourseList.css';
 
 function CourseList({ courses = [] }) {
   return (
