5396af3e29394a1e46bb2144ff0e8fea6aa24f22
savvyh <7277@holbertonstudents.com>
2025-11-26 14:25:50 +0000

    style: add a wrapper to make it responsive

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -5,28 +5,30 @@ import './CourseList.css';
 
 function CourseList({ courses = [] }) {
   return (
-    <table id="CourseList">
-      {courses.length > 0 && (
-        <thead>
-          <CourseListRow textFirstCell="Available courses" isHeader={true} />
-          <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
-        </thead>
-      )}
-      <tbody>
-        {courses.length === 0 ? (
-          <CourseListRow textFirstCell="No course available yet" isHeader={false} />
-        ) : (
-          courses.map((course) => (
-            <CourseListRow
-              key={course.id}
-              textFirstCell={course.name}
-              textSecondCell={course.credit}
-              isHeader={false}
-            />
-          ))
+    <div className="w-[80%] mx-auto">
+      <table id="CourseList" className="w-full">
+        {courses.length > 0 && (
+          <thead>
+            <CourseListRow textFirstCell="Available courses" isHeader={true} />
+            <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+          </thead>
         )}
-      </tbody>
-    </table>
+        <tbody>
+          {courses.length === 0 ? (
+            <CourseListRow textFirstCell="No course available yet" isHeader={false} />
+          ) : (
+            courses.map((course) => (
+              <CourseListRow
+                key={course.id}
+                textFirstCell={course.name}
+                textSecondCell={course.credit}
+                isHeader={false}
+              />
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
