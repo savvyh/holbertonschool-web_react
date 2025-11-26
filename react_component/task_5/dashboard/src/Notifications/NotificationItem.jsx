c98663ebc7bdfad930994b5719277956b927840b
savvyh <7277@holbertonstudents.com>
2025-11-26 09:09:03 +0000

    refactor: change NotificationItem component to extend PureComponent for performance optimization

--- react_component/task_5/dashboard/src/Notifications/NotificationItem.jsx
+++ react_component/task_5/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-class NotificationItem extends Component {
+class NotificationItem extends PureComponent {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
