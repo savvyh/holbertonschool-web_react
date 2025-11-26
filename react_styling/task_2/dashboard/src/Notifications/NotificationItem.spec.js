84218b2f17f4b830dbff6c219ec9143e4462e87c
savvyh <7277@holbertonstudents.com>
2025-11-26 15:43:59 +0000

    feat: remove useless style test cause now I use tailwind for this task

--- react_styling/task_2/dashboard/src/Notifications/NotificationItem.spec.js
+++ react_styling/task_2/dashboard/src/Notifications/NotificationItem.spec.js
@@ -2,30 +2,6 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import NotificationItem from './NotificationItem';
 
 describe('NotificationItem Component', () => {
-  test('renders li element with blue color and data-notification-type="default" when type is "default"', () => {
-    const { container } = render(
-      <NotificationItem type="default" value="Test notification" />
-    );
-    
-    const liElement = container.querySelector('li');
-    
-    expect(liElement).toHaveStyle({ color: 'rgb(0, 0, 255)' });
-    
-    expect(liElement).toHaveAttribute('data-notification-type', 'default');
-  });
-
-  test('renders li element with red color and data-notification-type="urgent" when type is "urgent"', () => {
-    const { container } = render(
-      <NotificationItem type="urgent" value="Urgent notification" />
-    );
-    
-    const liElement = container.querySelector('li');
-    
-    expect(liElement).toHaveStyle({ color: 'rgb(255, 0, 0)' });
-    
-    expect(liElement).toHaveAttribute('data-notification-type', 'urgent');
-  });
-
   test('renders value text correctly', () => {
     render(
       <NotificationItem type="default" value="Test notification text" />
