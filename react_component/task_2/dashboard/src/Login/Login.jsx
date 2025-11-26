fcafd222a9a15182f1a9e07c8a28ff984c35603c
savvyh <7277@holbertonstudents.com>
2025-11-26 08:12:10 +0000

    feat: init task 2

---
+++ react_component/task_2/dashboard/src/Login/Login.jsx
@@ -0,0 +1,19 @@
+import './Login.css';
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="App-body">
+      <p>Login to access the full dashboard</p>
+      <form>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password" />
+        <button type="button">OK</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
