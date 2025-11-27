9c8a7b78157df621a3f50f5eaeba6005e5416d39
savvyh <7277@holbertonstudents.com>
2025-11-27 14:25:26 +0000

    style: improve header to match with layout

--- react_styling/task_3/dashboard/src/Header/Header.jsx
+++ react_styling/task_3/dashboard/src/Header/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className="flex items-center p-5 border-b-[3px] border-main-color">
+    <div className="flex items-center py-2">
       <img src={logo} alt="holberton logo" className="w-[200px] h-auto mr-5"/>
-      <h1 className="text-[2rem] text-main-color">School dashboard</h1>
+      <h1 className="text-[2rem] text-main-color font-bold">School dashboard</h1>
     </div>
   );
 }
