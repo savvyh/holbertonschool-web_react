fcaa7b9ec282be6c90d43985acb0fb2aa39c01c3
savvyh <7277@holbertonstudents.com>
2025-11-27 14:25:51 +0000

    refactor: remove Login.css and update Login component to use Tailwind CSS for styling

--- react_styling/task_3/dashboard/src/Login/Login.jsx
+++ react_styling/task_3/dashboard/src/Login/Login.jsx
@@ -1,17 +1,16 @@
-import './Login.css';
 import React from 'react';
 
 function Login() {
   return (
-    <div className="App-body">
-      <p>Login to access the full dashboard</p>
-      <form>
-        <label htmlFor="email">Email: </label>
-        <input type="email" id="email" name="email" />
-        <label htmlFor="password">Password: </label>
-        <input type="password" id="password" name="password" />
-        <button type="button">OK</button>
-      </form>
+    <div className="flex flex-col p-5 pl-10 h-[45vh] border-t-4 border-main-color">
+      <p className="text-xl">Login to access the full dashboard</p>
+      <div className="mt-8 text-lg">
+        <label htmlFor="email" className="pr-2">Email</label>
+        <input type="email" name="user_email" id="email" className="border rounded" />
+        <label htmlFor="password" className="pl-2 pr-2">Password</label>
+        <input type="password" name="user_password" id="password" className="border rounded" />
+        <button className="cursor-pointer border px-1 rounded ml-2">OK</button>
+      </div>
     </div>
   );
 }
