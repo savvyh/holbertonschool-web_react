a6a5c8643c41ae79a139590d17a38ababa5d9cf2
savvyh <7277@holbertonstudents.com>
2025-11-27 10:46:18 +0000

    feat: use tailwind instead of css for header

--- react_styling/task_3/dashboard/src/Header/Header.jsx
+++ react_styling/task_3/dashboard/src/Header/Header.jsx
@@ -1,12 +1,11 @@
-import './Header.css';
 import logo from '../assets/holberton-logo.jpg';
 import React from 'react';
 
 function Header() {
   return (
-    <div className="App-header">
-      <img src={logo} alt="holberton logo" />
-      <h1>School dashboard</h1>
+    <div className="flex items-center p-5 border-b-[3px] border-main-color">
+      <img src={logo} alt="holberton logo" className="w-[200px] h-auto mr-5"/>
+      <h1 className="text-[2rem] text-main-color">School dashboard</h1>
     </div>
   );
 }
