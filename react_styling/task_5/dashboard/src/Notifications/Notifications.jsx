58c89f7c1d33787f23763bd24f47d02916f5afab
savvyh <7277@holbertonstudents.com>
2025-11-28 12:41:07 +0000

    refactor: simplify Notifications component by removing unnecessary methods and improving prop handling

--- react_styling/task_5/dashboard/src/Notifications/Notifications.jsx
+++ react_styling/task_5/dashboard/src/Notifications/Notifications.jsx
@@ -1,57 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import closeIcon from '../assets/close-button.png';
 import NotificationItem from './NotificationItem';
 
-class Notifications extends Component {
+class Notifications extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClose = this.handleClose.bind(this);
     this.markAsRead = this.markAsRead.bind(this);
   }
 
   shouldComponentUpdate(nextProps) {
     return (
-      nextProps.notifications.length !== this.props.notifications.length ||
-      nextProps.displayDrawer !== this.props.displayDrawer
+      this.props.notifications.length !== nextProps.notifications.length ||
+      this.props.displayDrawer !== nextProps.displayDrawer
     );
   }
 
-  handleClose() {
-    console.log('Close button has been clicked');
-  }
-
   markAsRead(id) {
-    console.log(`Notification ${id} has been marked as read`);
+    console.log(`Notification ${id + 1} has been marked as read`);
   }
 
   render() {
-    const { notifications, displayDrawer } = this.props;
+    const { displayDrawer = false, notifications = [] } = this.props;
+
     const shouldBounce = notifications.length > 0 && !displayDrawer;
 
     return (
       <>
-        <div className={`absolute right-3 top-1 whitespace-nowrap ${shouldBounce ? 'animate-bounce' : ''}`}>
+        <div className={`notification-title absolute right-3 top-1 whitespace-nowrap ${shouldBounce ? 'animate-bounce' : ''}`}>
           Your notifications
         </div>
-        {displayDrawer && (
-          <div className="relative border-[3px] border-dotted border-[color:var(--main-color)] right-3 p-1.5 w-[380px] float-right mt-7 max-[912px]:w-full max-[912px]:fixed max-[912px]:top-0 max-[912px]:left-0 max-[912px]:right-0 max-[912px]:bottom-0 max-[912px]:z-50 max-[912px]:float-none max-[912px]:m-0 max-[912px]:p-3 max-[912px]:bg-white max-[912px]:overflow-y-hidden max-[912px]:h-screen max-[430px]:overflow-y-hidden max-[430px]:h-screen">
+        {displayDrawer ? (
+          <div className="notification-items relative border-[3px] border-dotted border-[color:var(--main-color)] right-3 p-1.5 w-[380px] float-right mt-7 max-[912px]:w-full max-[912px]:fixed max-[912px]:top-0 max-[912px]:left-0 max-[912px]:right-0 max-[912px]:bottom-0 max-[912px]:z-50 max-[912px]:float-none max-[912px]:m-0 max-[912px]:p-3 max-[912px]:bg-white max-[912px]:overflow-y-hidden max-[912px]:h-screen max-[430px]:overflow-y-hidden max-[430px]:h-screen">
             {notifications.length > 0 ? (
               <div className="relative">
                 <p className="m-0 max-[912px]:text-[20px]">Here is the list of notifications</p>
                 <button
-                  type="button"
-                  className="absolute cursor-pointer right-0 top-0 bg-transparent border-0"
-                  onClick={this.handleClose}
+                  onClick={() => console.log('Close button has been clicked')}
                   aria-label="Close"
+                  className="absolute cursor-pointer right-0 top-0 bg-transparent"
                 >
                   <img src={closeIcon} alt="close icon" className="w-3 h-3" />
                 </button>
                 <ul className="list-[square] pl-5 max-[912px]:p-0 max-[912px]:list-none">
-                  {notifications.map((notification) => (
+                  {notifications.map((notification, index) => (
                     <NotificationItem
+                      id={index}
                       key={notification.id}
-                      id={notification.id}
                       type={notification.type}
                       value={notification.value}
                       html={notification.html}
@@ -64,13 +59,14 @@ class Notifications extends Component {
               <p className="max-[912px]:text-[20px]">No new notification for now</p>
             )}
           </div>
-        )}
+        ) : null}
       </>
     );
   }
 }
 
 Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
   notifications: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
@@ -80,13 +76,12 @@ Notifications.propTypes = {
         __html: PropTypes.string
       })
     })
-  ),
-  displayDrawer: PropTypes.bool
+  )
 };
 
 Notifications.defaultProps = {
-  notifications: [],
-  displayDrawer: true
+  displayDrawer: false,
+  notifications: []
 };
 
 export default Notifications;
