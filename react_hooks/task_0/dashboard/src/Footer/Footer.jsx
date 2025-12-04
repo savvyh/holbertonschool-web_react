c38044ddcfe582d62a797e2f8ef1fc386f2cc5cf
savvyh <7277@holbertonstudents.com>
2025-12-04 10:00:25 +0000

    refactor: simplify Footer component by using useContext for user state management

--- react_hooks/task_0/dashboard/src/Footer/Footer.jsx
+++ react_hooks/task_0/dashboard/src/Footer/Footer.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { getCurrentYear, getFooterCopy } from '../utils/utils';
 import AppContext from '../Context/context';
 
 function Footer() {
+  const { user } = useContext(AppContext);
+
   return (
-    <AppContext.Consumer>
-      {({ user }) => (
         <div className="border-t-[3px] border-[#e1003c] p-5">
           <p className="text-center italic text-sm md:text-base">Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
           {user.isLoggedIn && (
             <p className="text-center">
               <a href="#">Contact us</a>
             </p>
-          )}
-        </div>
-      )}
-    </AppContext.Consumer>
+        )}
+      </div>
   );
 }
 
