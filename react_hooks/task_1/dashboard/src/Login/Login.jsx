3e442644db2b95217298ae532edbabad2f846f0e
savvyh <7277@holbertonstudents.com>
2025-12-05 09:47:08 +0000

    refactor: convert Login component from class to functional component using hooks for state management

--- react_hooks/task_1/dashboard/src/Login/Login.jsx
+++ react_hooks/task_1/dashboard/src/Login/Login.jsx
@@ -1,64 +1,53 @@
-import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    const { email = '', password = '' } = props;
-    this.state = {
-      email,
-      password,
-      enableSubmit: false
-    };
-    this.handleChangeEmail = this.handleChangeEmail.bind(this);
-    this.handleChangePassword = this.handleChangePassword.bind(this);
-    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
-  }
+function Login ({logIn, email: loginEmail = '', password: loginPassword = ''}) {
+  const [formData, setFormData] = useState({ 
+    email: loginEmail, 
+    password: loginPassword 
+  });
+  const [enableSubmit, setEnableSubmit] = useState(false)
 
-  handleChangeEmail(event) {
-    const email = event.target.value;
-    this.setState({ email }, () => {
-      this.checkEnableSubmit();
-    });
-  }
-
-  handleChangePassword(event) {
-    const password = event.target.value;
-    this.setState({ password }, () => {
-      this.checkEnableSubmit();
-    });
-  }
-
-  checkEnableSubmit() {
-    const { email, password } = this.state;
+  const checkEnableSubmit = (currentEmail, currentPassword) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const isValidEmail = emailRegex.test(email);
-    const isValidPassword = password.length >= 8;
-    const enableSubmit = isValidEmail && isValidPassword;
-    this.setState({ enableSubmit });
+    const isValidEmail = emailRegex.test(currentEmail);
+    const isValidPassword = currentPassword.length >= 8;
+    return isValidEmail && isValidPassword;
   }
 
-  handleLoginSubmit(event) {
+
+  const handleChangeEmail = (event) => {
+    const newEmail = event.target.value;
+    setFormData({ ...formData, email: newEmail });
+
+    const isValidEmail = checkEnableSubmit(newEmail, formData.password);
+    setEnableSubmit(isValidEmail);
+  }
+
+  const handleChangePassword = (event) => {
+    const newPassword = event.target.value;
+    setFormData({ ...formData, password: newPassword });
+
+    const isValidPassword = checkEnableSubmit(formData.email, newPassword);
+    setEnableSubmit(isValidPassword);
+  }
+
+  const handleLoginSubmit = (event) => {
     event.preventDefault();
-    const { logIn } = this.props;
-    const { email, password } = this.state;
-    logIn(email, password);
+    logIn(formData.email, formData.password);
   }
-
-  render() {
-    const { email, password, enableSubmit } = this.state;
 
     return (
       <div className="flex flex-col p-5 pl-10 h-[45vh] border-t-4 border-main-color">
         <p className="text-xl">Login to access the full dashboard</p>
-        <form onSubmit={this.handleLoginSubmit} className="mt-8 text-lg flex flex-col sm:block">
+        <form onSubmit={handleLoginSubmit} className="mt-8 text-lg flex flex-col sm:block">
           <label htmlFor="email" className="pr-2">Email</label>
           <input
             type="email"
             name="user_email"
             id="email"
-            value={email}
-            onChange={this.handleChangeEmail}
+            value={formData.email}
+            onChange={handleChangeEmail}
             className="border rounded w-full sm:w-auto"
           />
           <label htmlFor="password" className="pl-0 sm:pl-2 pr-2 mt-2 sm:mt-0">Password</label>
@@ -66,8 +55,8 @@ class Login extends React.Component {
             type="password"
             name="user_password"
             id="password"
-            value={password}
-            onChange={this.handleChangePassword}
+            value={formData.password}
+            onChange={handleChangePassword}
             className="border rounded w-full sm:w-auto"
           />
           <input
@@ -80,7 +69,6 @@ class Login extends React.Component {
       </div>
     );
   }
-}
 
 Login.propTypes = {
   logIn: PropTypes.func,
