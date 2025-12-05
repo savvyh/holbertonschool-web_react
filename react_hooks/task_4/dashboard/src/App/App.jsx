1250e7f69838b90b2bbd480cc6fd94fdcc25ae0d
savvyh <7277@holbertonstudents.com>
2025-12-05 14:04:05 +0000

    fix: remove contextValue

--- react_hooks/task_4/dashboard/src/App/App.jsx
+++ react_hooks/task_4/dashboard/src/App/App.jsx
@@ -77,13 +77,8 @@ function App() {
     };
   }, []);
 
-  const contextValue = {
-    user,
-    logOut
-  };
-
   return (
-    <AppContext.Provider value={contextValue}>
+    <AppContext.Provider value={{ user: user, logOut: logOut }}>
       <div className="min-h-screen flex flex-col m-0">
         <div className="absolute top-0 right-0 z-10">
           <Notifications
