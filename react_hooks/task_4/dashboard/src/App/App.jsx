3b305726f5b2d2ba2094a4d134dedd975f0b9363
savvyh <7277@holbertonstudents.com>
2025-12-05 13:09:20 +0000

    refactor: convert App component from class to functional component using hooks for state management

--- react_hooks/task_4/dashboard/src/App/App.jsx
+++ react_hooks/task_4/dashboard/src/App/App.jsx
@@ -1,150 +1,107 @@
-import React, { Fragment, Component } from 'react';
-import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
-import Header from '../Header/Header';
 import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
+import WithLogging from '../HOC/WithLogging';
+import React, { useState } from 'react';
+import AppContext from '../Context/context';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
 import BodySection from '../BodySection/BodySection';
 import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
-import WithLogging from '../HOC/WithLogging';
-import AppContext from '../Context/context';
+import Footer from '../Footer/Footer';
+
 
 const CourseListWithLogging = WithLogging(CourseList);
 const LoginWithLogging = WithLogging(Login);
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
+function App() {
+  const notificationsList = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } }
+  ];
 
-    const notificationsList = [
-      { id: 1, type: 'default', value: 'New course available' },
-      { id: 2, type: 'urgent', value: 'New resume available' },
-      { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } }
-    ];
+  const coursesList = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 }
+  ];
 
-    const coursesList = [
-      { id: 1, name: 'ES6', credit: 60 },
-      { id: 2, name: 'Webpack', credit: 20 },
-      { id: 3, name: 'React', credit: 40 }
-    ];
+  const [displayDrawer, setDisplayDrawer] = useState(true)
+  const [user, setUser] = useState({email: '', password: '', isLoggedIn: false})
+  const [notifications, setNotifications] = useState(notificationsList);
+  const [courses, setCourses] = useState(coursesList);
 
-    this.state = {
-      displayDrawer: false,
-      user: {
-        email: '',
-        password: '',
-        isLoggedIn: false
-      },
-      logOut: () => {},
-      notifications: notificationsList,
-      courses: coursesList
-    };
-
-    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
-    this.handleHideDrawer = this.handleHideDrawer.bind(this);
-    this.logIn = this.logIn.bind(this);
-    this.logOut = this.logOut.bind(this);
-    this.markNotificationAsRead = this.markNotificationAsRead.bind(this);
-
-    this.state.logOut = this.logOut;
+  const logIn = (email, password) => {
+    setUser({
+      email: email,
+      password: password,
+      isLoggedIn: true,
+    })
   }
 
-  logIn(email, password) {
-    this.setState({
-      user: {
-        email,
-        password,
-        isLoggedIn: true
-      }
-    });
+  const logOut = () => {
+    setUser({
+      email: '',
+      password: '',
+      isLoggedIn: false,
+    })
   }
 
-  logOut() {
-    this.setState({
-      user: {
-        email: '',
-        password: '',
-        isLoggedIn: false
-      }
-    });
+  const handleDisplayDrawer = () => {
+    setDisplayDrawer(true);
   }
 
-  handleDisplayDrawer() {
-    this.setState({ displayDrawer: true });
+  const handleHideDrawer = () => {
+    setDisplayDrawer(false);
   }
 
-  handleHideDrawer() {
-    this.setState({ displayDrawer: false });
-  }
-
-  markNotificationAsRead(id) {
+  const markNotificationAsRead = (id) => {
     console.log(`Notification ${id} has been marked as read`);
-    this.setState(prevState => ({
-      notifications: prevState.notifications.filter(notification => notification.id !== id)
-    }));
+    setNotifications(prevNotifications  => (
+      prevNotifications.filter(notification => notification.id !== id)
+    ));
   }
 
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyDown);
-  }
+  const contextValue = {
+    user,
+    logOut
+  };
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown(event) {
-    if (event.ctrlKey && event.key === 'h') {
-      event.preventDefault();
-      alert('Logging you out');
-      this.logOut();
-    }
-  }
-
-  render() {
-    const { displayDrawer, user, logOut, notifications, courses } = this.state;
-
-    const contextValue = {
-      user,
-      logOut
-    };
-
-    return (
-      <AppContext.Provider value={contextValue}>
-        <Fragment>
-          <div className="min-h-screen flex flex-col m-0">
-            <div className="absolute top-0 right-0 z-10">
-              <Notifications 
-                notifications={notifications} 
-                displayDrawer={displayDrawer} 
-                handleDisplayDrawer={this.handleDisplayDrawer} 
-                handleHideDrawer={this.handleHideDrawer}
-                markNotificationAsRead={this.markNotificationAsRead}
-              />
-            </div>
-            <Header />
-            <div className="flex-1 px-4 md:px-8">
-              {user.isLoggedIn ? (
-                <BodySectionWithMarginBottom title="Course list">
-                  <CourseListWithLogging courses={courses} />
-                </BodySectionWithMarginBottom>
-              ) : (
-                <BodySectionWithMarginBottom title="Log in to continue">
-                  <LoginWithLogging logIn={this.logIn} email={user.email} password={user.password} />
-                </BodySectionWithMarginBottom>
-              )}
-              <BodySection title="News from the School">
-                <p>ipsum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, asperiores architecto blanditiis fuga doloribus sit illum aliquid ea distinctio minus accusantium, impedit quo voluptatibus ut magni dicta. Recusandae, quia dicta?</p>
-              </BodySection>
-            </div>
-            <Footer />
+  return (
+    <AppContext.Provider value={contextValue}>
+      <Fragment>
+        <div className="min-h-screen flex flex-col m-0">
+          <div className="absolute top-0 right-0 z-10">
+            <Notifications 
+              notifications={notifications} 
+              displayDrawer={displayDrawer} 
+              handleDisplayDrawer={handleDisplayDrawer} 
+              handleHideDrawer={handleHideDrawer}
+              markNotificationAsRead={markNotificationAsRead}
+            />
           </div>
-        </Fragment>
-      </AppContext.Provider>
-    );
-  }
+          <Header />
+          <div className="flex-1 px-4 md:px-8">
+            {user.isLoggedIn ? (
+              <BodySectionWithMarginBottom title="Course list">
+                <CourseListWithLogging courses={courses} />
+              </BodySectionWithMarginBottom>
+            ) : (
+              <BodySectionWithMarginBottom title="Log in to continue">
+                <LoginWithLogging logIn={logIn} email={user.email} password={user.password} />
+              </BodySectionWithMarginBottom>
+            )}
+            <BodySection title="News from the School">
+              <p>ipsum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, asperiores architecto blanditiis fuga doloribus sit illum aliquid ea distinctio minus accusantium, impedit quo voluptatibus ut magni dicta. Recusandae, quia dicta?</p>
+            </BodySection>
+          </div>
+          <Footer />
+        </div>
+      </Fragment>
+    </AppContext.Provider>
+  );
 }
 
 App.propTypes = {};
 
-export default App;
+export default React.memo(App);
