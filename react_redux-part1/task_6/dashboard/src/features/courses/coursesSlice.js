3639828fd2f4d95e58a4d62c0b542faa59d95ea5
savvyh <7277@holbertonstudents.com>
2026-01-16 08:43:44 +0000

    feat: init task 6

---
+++ react_redux-part1/task_6/dashboard/src/features/courses/coursesSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { logout } from '../auth/authSlice';
+
+const initialState = {
+  courses: [],
+};
+
+const API_BASE_URL = 'http://localhost:5173';
+const ENDPOINTS = {
+  courses: `${API_BASE_URL}/courses.json`,
+};
+
+export const fetchCourses = createAsyncThunk('courses/fetchCourses', async () => {
+  const response = await fetch(ENDPOINTS.courses);
+  return response.json();
+});
+
+const coursesSlice = createSlice({
+  name: 'courses',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCourses.fulfilled, (state, action) => {
+        state.courses = action.payload;
+      })
+      .addCase(logout, (state) => {
+        state.courses = [];
+      });
+  },
+});
+
+export default coursesSlice.reducer;
